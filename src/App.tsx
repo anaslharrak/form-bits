02e4e186bd44aca61fcc3976cb04aa7b31cd935b
Anas Lharrak <146816560+anaslharrak@users.noreply.github.com>
2024-07-05 21:35:24 +0200

    feat: :sparkles: add react router
    added react router in order to improve the navigation of the game (WIP: handle redux and react router)

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import './App.css'
+import { ChooseForm, Game, HomePage } from './components/exports'
+import {Routes, Route } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { RootState } from './redux/store'
-import { ChooseForm, Game } from './components/exports'
 
 
 function App() {
-  const currentForm = useSelector((state: RootState) => state.form.form)
+  console.log(useSelector((state: RootState) => state))
   return (
-    <>
-      {
-        currentForm === ''
-          ? <ChooseForm />
-          : <Game />
-      }
-    </>
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/choose-form' element={<ChooseForm />} />
+      <Route path='/in-game' element={<Game />}  />
+    </Routes>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.ts'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>  
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
 )
