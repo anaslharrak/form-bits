0903409b13a566d6198bdb05cceb9f61b6318e06
Anas Lharrak <146816560+anaslharrak@users.noreply.github.com>
2024-07-05 21:49:59 +0200

    feat: :construction: Wireframe home page
    added some simple HTML in order to do a simple wireframe of the home page

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,7 +1,22 @@
 const HomePage =  () => {
     return (
         <div>
-            <h1>Home Page</h1>
+            <h1>¡Welcome to FormBites!</h1>
+            <div>
+                <h3>Descrpition</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus laboriosam fuga minus eius quaerat non dicta molestias esse aspernatur earum doloribus quos veritatis consequatur omnis at libero nam, ipsam voluptatem!</p>
+            </div>
+
+            <div>
+                <button>
+                    PLAY
+                </button>
+
+                <button>
+                    TUTORIAL
+                </button>
+            </div>
+
         </div>
     );
 }
