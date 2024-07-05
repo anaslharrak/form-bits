206626d43f78242b0b10487b6d1f228d7c304c1a
Anas Lharrak <146816560+anaslharrak@users.noreply.github.com>
2024-07-05 21:43:12 +0200

    feat: :fire: add react router and redux
    fixed compatibility with redux and  react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import './App.css'
 import { ChooseForm, Game, HomePage } from './components/exports'
-import {Routes, Route } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { RootState } from './redux/store'
+import {Routes, Route, Link } from 'react-router-dom'
 
 
 function App() {
-  console.log(useSelector((state: RootState) => state))
+  
   return (
+    <>
+
+    <Link to='/'>Home</Link>
+    <Link to='/choose-form'>Choose form</Link>
+    <Link to='/in-game'>Game</Link>
+    
     <Routes>
       <Route path='/' element={<HomePage />} />
       <Route path='/choose-form' element={<ChooseForm />} />
       <Route path='/in-game' element={<Game />}  />
     </Routes>
+
+    </>
   )
 }
 
--- src/components/exports.ts
+++ src/components/exports.ts
@@ -1,3 +1,4 @@
 export { default as ChooseForm } from "./ChooseForm/ChooseForm";
 export { default as Game } from "./Game/Game";
 export { default as BasicCard } from "./BasicCard/BasicCard";
+export { default as HomePage } from "./HomePage/HomePage";
